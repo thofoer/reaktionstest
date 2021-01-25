ec308eb7e0b2eddb31e879195ec56eae628c614d
thofoer <thofoer@gmx.de>
2021-01-25 19:04:49 +0100

    LED-Bar

---
+++ reaktionstest.ino
@@ -0,0 +1,57 @@
+
+
+TaskHandle_t blinkerTask;
+
+const int  barPins[] = { 12, 14, 27, 26, 25, 33, 32, 13, 17, 16 };
+
+void setup() {
+   Serial.begin(115200); 
+
+   for (int i=0; i<10; i++) {
+      pinMode(barPins[i], OUTPUT);
+   }
+  
+   xTaskCreatePinnedToCore(
+      blinker, 
+      "blinker",
+      10000,  
+      NULL,  
+      0,  
+      &blinkerTask,
+      0);     
+}
+
+void loop() {
+
+  for (int i=0; i<10; i+=2) {
+      digitalWrite(barPins[i], HIGH);
+  }
+   
+  delay(500);
+  
+ for (int i=0; i<10; i+=2) {
+      digitalWrite(barPins[i], LOW);
+  }
+  delay(500);
+
+   Serial.print("Task A running on core ");
+  Serial.println(xPortGetCoreID());
+}
+
+
+void blinker( void * parameter) {
+  for(;;) {
+    for (int i=1; i<10; i+=2) {
+      digitalWrite(barPins[i], HIGH);
+  }
+   
+    delay(331);
+       for (int i=1; i<10; i+=2) {
+      digitalWrite(barPins[i], LOW);
+  }
+    delay(331);   
+
+    Serial.print("Task B running on core ");
+  Serial.println(xPortGetCoreID());
+  }
+}
